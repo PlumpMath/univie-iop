e4c4e51b463ec56641ce202dd8646a4972270103
rhoerbe <rainer@hoerbe.at>
2015-04-06 09:51:43 +0200

    refactored

--- Uebung2/make_hierarchy.py
+++ Uebung2/make_hierarchy.py
@@ -9,24 +9,28 @@ def prettifyXML(elem):
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent=" ")
 
-renameTag = {
-    'level2' : 'studybranch',
-    'level3' : 'study',
-    'level4' : 'modulegroup',
-    'level5' : 'modulesubgroup',  # module if no subsequent level6
-    'level6' : 'module',
-}
-currentLevel5Type = None  # either leaf or node
+def rename_element(e, currLevel, nextLevel):
+    renameTag = {
+        'level2' : 'studybranch',
+        'level3' : 'study',
+        'level4' : 'modulegroup',
+        'level5' : 'modulesubgroup',  # module if no subsequent level6
+        'level6' : 'module',
+    }
+    if currLevel == 5 and nextLevel != 6:
+        e.tag = renameTag['level6']  # no modulesubgroup
+    elif currLevel == 5 and nextLevel == 6:
+        e.tag = renameTag['level5']
+    else:
+        e.tag = renameTag[e.tag]
 
-outroot = ElementTree.Element('lectureindex')
-currentElem = [outroot] # stack mirrors hierarchy of current hierarchy
 def add_contents(e):
     anchor = e.attrib['anker']
     #if anchor:
     #    e.set('anchor1', anchor)
     xpath = ('.//*[@id="%s"]' % anchor[1:])
     ##print(xpath, end="")
-    chap = rootElem.find(xpath)
+    chap = inRoot.find(xpath)
     #if chap:
     #    e.append(chap)
 
@@ -38,34 +42,26 @@ def new_level(e):
     add_contents(e)
 
 levelList = []
-rootElem = ElementTree.parse('vlz_kap_706 frag.xml').getroot()
-for e in rootElem.findall('*'):
+inRoot = ElementTree.parse('vlz_kap_706 frag.xml').getroot()
+for e in inRoot.findall('*'):
     if e.tag.startswith('level'):
         levelList.append(e)
 levelList.append(ElementTree.Element('level0')) # terminating element
     
-''' 3 jobs in one:
-1. create hierarchy from linear list by inserting close tags at the appropriate location
-2. rename levelN tag to specific name (see renameTag)
-3. skip level5 modulesubgroups if no level6 existing:
-   a new level is a module if not level6 follows (i.e. "leaf")
-   a level 4 must close the previous level 5 according to its type
+''' 1. create hierarchy from linear list by inserting close tags at the appropriate location
+    2. skip level5 modulesubgroups if no level6 existing:
+       a new level is a module if no level6 follows (i.e. "leaf")
+       a level 4 must close the previous level 5 according to its type
 '''
+outRoot = ElementTree.Element('lectureindex')
+currentElem = [outRoot] # stack mirrors hierarchy of current hierarchy
 levelStack = [0]
 prevLevel = 0
-currentLevel5Type = None
 for pos, e in enumerate(levelList):
     if pos == len(levelList) - 1: break
     currLevel = int(e.tag[-1])  # get last char - lt 10 levels in input
     nextLevel = int(levelList[pos+1].tag[-1])
-    if currLevel == 5 and nextLevel != 6:
-        e.tag = renameTag['level6']  # no modulesubgroup
-        currentLevel5Type = 'leaf'
-    elif currLevel == 5 and nextLevel == 6:
-        e.tag = renameTag['level5']
-        currentLevel5Type = 'node'
-    else:
-        e.tag = renameTag[e.tag]
+    rename_element(e, currLevel, nextLevel)
     prevLevel = int(levelStack[-1]) # get last element in list
     if currLevel == prevLevel:
         currentElem.pop()
@@ -80,6 +76,6 @@ for pos, e in enumerate(levelList):
             currentElem.pop()
         new_level(e)
 
-outTree = ElementTree.ElementTree(outroot)
+outTree = ElementTree.ElementTree(outRoot)
 outTree.write('z.xml', encoding='utf-8')
-print(prettifyXML(outroot))
+print(prettifyXML(outRoot))
