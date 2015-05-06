9f306c17e6ee003a524f5bf57a02d07ea8064cca
Rainer Hörbe <rhoerbe@users.noreply.github.com>
2015-05-06 17:36:12 +0200

    SOAP fault thrown for unknown currency

--- Uebung3/xconv/soap_server.php
+++ Uebung3/xconv/soap_server.php
@@ -1,9 +1,19 @@
 <?php
   class XConvService {
-    function getXConv($currency) {
-      $XConv = new StdClass;
-      $XConv->convertedAmount = $currency / 1.04;
-      return $XConv->convertedAmount;
+    function getXConv($currency, $amount) {
+      $xrates = array(
+          "USD" => 1.04,
+          "JPY" => 93.22,
+          "CNY" => 2.04,
+          "CHF" => 1.08,
+      );    
+      if (array_key_exists($currency, $xrates)) {
+          $XConv = new StdClass;
+          $XConv->convertedAmount = $currency / 1.04;
+          return $XConv->convertedAmount;
+      } else {
+          return new SoapFault('100', 'Currency not supported', 'Callee', 'see list of supported currencies', 'UnsupportedCurrency');      
+      }
     }
   }
  
